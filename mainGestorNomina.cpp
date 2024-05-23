f0e7b4852aa32e46bdfc5b13f60571e763808eba
chicho377 <morafabricio86@gmail.com>
2024-05-22 22:38:35 -0600

    Se añadio el menu con el ciclo do while

--- mainGestorNomina.cpp
+++ mainGestorNomina.cpp
@@ -22,6 +22,26 @@ int main(int argc, char** argv) {
     int cantOperarios = 0, cantTecnicos = 0, cantProfesionales = 0;
     double acumuladoNetoOperarios = 0, acumuladoNetoTecnicos = 0, acumuladoNetoProfesionales = 0;
     
+    do{
+    	printf("Ingrese la cedula del empleado: ");
+        scanf("%s", cedula);
+
+        printf("Ingrese el nombre del empleado: ");
+        scanf("%s", nombre);
+
+        printf("Ingrese el tipo de empleado (1-Operario, 2-Tecnico, 3-Profesional): ");
+        scanf("%d", &tipo);
+
+        printf("Ingrese el salario por hora: ");
+        scanf("%lf", &salarioPorHora);
+
+        printf("Ingrese la cantidad de horas trabajadas: ");
+        scanf("%lf", &horasTrabajadas);
+        
+        printf("\n¿Desea ingresar otro empleado? (1-Si, 0-No): ");
+        scanf("%d", &opcion);
+	}while(opcion != 0);
+    
     
 	return 0;
 }
