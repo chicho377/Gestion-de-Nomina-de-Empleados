571771a440b71f200865267325126eedd40778cf
chicho377 <morafabricio86@gmail.com>
2024-05-22 22:30:32 -0600

    Se añadieron archivos del proyecto, pseudocodigo, librerias y variables

---
+++ mainGestorNomina.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	//Variables
+	char cedula[20];
+    char nombre[50];
+    int tipo;
+    double salarioPorHora;
+    double horasTrabajadas;
+    double salarioOrdinario;
+    double aumento;
+    double salarioBruto;
+    double deduccionCCSS;
+    double salarioNeto;
+
+    int opcion;
+    int cantOperarios = 0, cantTecnicos = 0, cantProfesionales = 0;
+    double acumuladoNetoOperarios = 0, acumuladoNetoTecnicos = 0, acumuladoNetoProfesionales = 0;
+    
+    
+	return 0;
+}
